862bad071b6f96023c46c4a05b8d1a2551c86ac6
rmlzy <rmlzy@outlook.com>
2021-04-12 11:42:04 +0800

    feat: cache sharedVars to redis

--- src/modules/public/public-view/public-view.service.ts
+++ src/modules/public/public-view/public-view.service.ts
@@ -3,21 +3,34 @@ import { md2html } from '../../../utils';
 import { OptionsService } from '../../options/options.service';
 import { ContentsService } from '../../contents/contents.service';
 import { IPaginate, IContent } from './interface/public-view.interface';
+import { RedisService } from 'nestjs-redis';
 
 @Injectable()
 export class PublicViewService {
   sharedVars = null;
 
-  constructor(private optionService: OptionsService, private contentService: ContentsService) {}
+  constructor(
+    private optionService: OptionsService,
+    private contentService: ContentsService,
+    private redisService: RedisService,
+  ) {}
 
   async findSharedVars() {
+    const redis = await this.redisService.getClient();
+    const sharedVars = await redis.get('sharedVars');
+    if (sharedVars) {
+      return JSON.parse(sharedVars);
+    }
+
     const siteConfig = await this.optionService.findSiteConfig();
     const pages = await this.contentService.findPages();
-    return {
+    const vars = {
       ...siteConfig,
       pages,
       theme: 'default',
     };
+    await redis.set('sharedVars', JSON.stringify(sharedVars));
+    return vars;
   }
 
   async findPosts(pageIndex): Promise<IPaginate<IContent>> {
